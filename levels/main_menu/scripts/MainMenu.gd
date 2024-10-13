ff54ccc00a63b6ede7ab8058cd142e179d41d21e
Devendra <tambatd@outlook.com>
2024-10-13 12:35:15 -0400

    added new pages and figured out a lot

--- levels/main_menu/scripts/MainMenu.gd
+++ levels/main_menu/scripts/MainMenu.gd
@@ -13,12 +13,25 @@ func _process(delta):
 
 func _on_load_intro_pressed():
 	print("loading into screen")
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://levels/intro_splash_screen/game_opening_splash.tscn")
+	return # Replace with function body.
 
 
 func _on_load_house_pressed():
+	print("loading house screen")
 	pass # Replace with function body.
 
 
 func _on_load_sound_test_pressed():
+	print("loading sound screen")
 	pass # Replace with function body.
+
+
+func _on_load_3D_car_test():
+	print("loading car test screen")
+	pass 
+	
+func _on_load_alanna_comicbook_test():
+	print("loading comicbook test screen")
+	pass 
+
