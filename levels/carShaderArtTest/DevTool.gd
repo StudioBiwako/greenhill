73569c16c75d0e92117ce7931229f731f246f721
Devendra <tambatd@outlook.com>
2024-10-25 23:59:00 -0400

    init devtool for pat

---
+++ levels/carShaderArtTest/DevTool.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		print('======')
+		print("right")
+		print('======')
+	
