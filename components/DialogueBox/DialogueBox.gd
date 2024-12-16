d4be80f7e2b5d7044983707769c34f6b46750d95
Devendra <tambatd@outlook.com>
2024-12-16 13:32:32 -0500

    init of Dialogue box

---
+++ components/DialogueBox/DialogueBox.gd
@@ -0,0 +1,18 @@
+extends Node2D
+#this script should read from controller input
+#to advance into the next dialgoue option
+# LINK TO RELEVANT CONCEPT ART
+# https://www.notion.so/UI-Elements-1208642730c3804fb979f4b130df0101
+
+# should consist of 4 elements
+# import 
+
+#on ready we should get the dialogue
+func _ready():
+	#
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ components/DialogueBox/TextBlock/TextBlock.gd
@@ -0,0 +1,12 @@
+extends Node2D
+#this should just be the indivudual text block
+#if bool of isCurrent color is blue, else grey
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
