9ecbbb412b80e18b09a02c9da22b2e1f10c9cb0a
Devendra <tambatd@outlook.com>
2024-12-17 22:05:53 -0500

    fixed buttons in main menu

--- levels/opening/main_menu/scripts/MainMenu.gd
+++ levels/opening/main_menu/scripts/MainMenu.gd
@@ -1,45 +1,38 @@
 extends Control
 var SavedGamesPanel = preload("res://levels/opening/main_menu/SavePanel/SavePanel.tscn")
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
 
 
 func _on_load_intro_pressed():
 	print("loading into screen")
 	get_tree().change_scene_to_file("res://levels/Intro/IntroManager.tscn")
-	return # Replace with function body.
-
-func _on_load_3D_car_test():
-	print("loading car test screen")
-	#var bgSound = SoundManager.get_currently_playing_music()
-	#if bgSound:
-		#bgSound = bgSound[0]
-		#bgSound.stop_music(3)
-	SoundManager.stop_music(3)
-	get_tree().change_scene_to_file("res://levels/carShaderArtTest/CarShaderArtTest.tscn")
-	pass 
+	return 
 
 func _on_load_credits_pressed():
 	SoundManager.stop_music(3)
 	print("loading car test screen")
-	pass # Replace with function body.
+	pass 
 
-
+'''
+Loads custom 3d test scene for testing purposes
+stops sound set from opening_manager
+'''
 func _on_load_3d_test_pressed():
-	pass # Replace with function body.
+	SoundManager.stop_music(3)
+	get_tree().change_scene_to_file("res://levels/carShaderArtTest/CarShaderArtTest.tscn")
+	pass 
 
 
-#Start Button Pressed
+#Start Button Pressed (don't kill audio on saved game panel openign up)
 func _on_start_game_pressed():
 	var SavedGamesPanelInstance = SavedGamesPanel.instantiate()
 	add_child(SavedGamesPanelInstance)
-	#get_tree().change_scene_to_file("res://levels/Intro/IntroManager.tscn")
-	pass # Replace with function body.
+	pass
