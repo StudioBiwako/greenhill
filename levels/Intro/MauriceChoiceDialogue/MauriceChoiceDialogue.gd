d3b83f5aaf38533a0e338efd21d0142a0d4aa1f9
Devendra <tambatd@outlook.com>
2024-12-19 12:14:26 -0500

    update

--- levels/Intro/MauriceChoiceDialogue/MauriceChoiceDialogue.gd
+++ levels/Intro/MauriceChoiceDialogue/MauriceChoiceDialogue.gd
@@ -9,3 +9,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+'''
+	both these buttons should send to 
+	the same scene after showing 
+	the 2nd maurice dialgoue choice
+''' 
+#on yes button
+func _on_yes_button_up():
+	get_tree().change_scene_to_file("res://levels/Intro/Void/Void.tscn")
+	pass # Replace with function body.
+
+#on no button
+func _on_no_button_up():
+	get_tree().change_scene_to_file("res://levels/Intro/Void/Void.tscn")
+	pass # Replace with function body.
